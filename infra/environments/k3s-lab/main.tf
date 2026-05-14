caf6970979f5248a36ca1e59131afe0a04b36f0c
tmmuniz <tmmuniz92@gmail.com>
2026-05-14 19:41:32 +0000

    Fix Network interface

--- infra/environments/k3s-lab/main.tf
+++ infra/environments/k3s-lab/main.tf
@@ -10,10 +10,11 @@ module "k3s_security_group" {
   source = "../../modules/k3s_security_group"
 
   project_name = var.project_name
-  vpc_id       = data.aws_instance.server.vpc_id
 
-  k3s_server_network_interface_id = data.aws_instance.server.primary_network_interface_id
-  k3s_agent_network_interface_id  = data.aws_instance.agent.primary_network_interface_id
+  vpc_id = data.aws_subnet.server.vpc_id
+  
+  k3s_server_network_interface_id = data.aws_instance.server.network_interface[0].network_interface_id
+  k3s_agent_network_interface_id  = data.aws_instance.agent.network_interface[0].network_interface_id
 
   allowed_public_ip_cidr = var.allowed_public_ip_cidr
 
